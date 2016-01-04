7cc6ce1893e648ba92c46b1c674a5913b02b4bc7
Stephanie Johnson <Stephanie.Johnson@ucsf.edu>
2016-01-04 14:19:43 -0800

    Real_data now takes a whole red trace instead of just a section; and the new file analyze_real_data is for 2-channel analysis

--- scripts/analyze_real_data.py
+++ scripts/analyze_real_data.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 import numpy.random as npr
 import matplotlib.pyplot as plt
-from scipy.io import loadmat # Steph add
+from scipy.io import loadmat
 
 from slopey.noise_models import make_gaussian_model
 from slopey.models import model1
@@ -12,20 +12,22 @@ npr.seed(0)  # reproducible
 
 
 ### load data
-# z = np.loadtxt('data/example_frames.txt')
-# Steph edit to load from mat file instead
-# z = loadmat('data/SNF2h103nMATP1mMHighFPS_2015_Dec04_Spot1_293_RedFr350to425.mat')
-#z = loadmat('data/SNF2h103nMATP1mMHighFPS_2015_Dec04_Spot1_187_RedFr400to466.mat')
-#z = z['OneRealSlopey'].ravel()
 z = loadmat('data/SNF2h103nMATP1mMHighFPS_2015_Dec04_Spot1_115_Results.mat')
-z = z['unsmoothedRedI'].ravel()
-num_frames = len(z)
+zR = z['unsmoothedRedI'].ravel()
+zG = z['unsmoothedGrI'].ravel()
+# Clip if necessary:
+start = z['start'].ravel()
+zR = zR[start[0]:sum(z['model_durations'].ravel())]
+zG = zG[start[0]:sum(z['model_durations'].ravel())]
+num_frames = len(zR)
 
 
 ### setting parameters
 
 # set how many slopey bits are expected
-num_slopey = 1
+# For now: get this info from the old HMM analysis
+# num_slopey = 2
+num_slopey = len(z['model_durations'].ravel())-1
 
 # set number of iterations of MH
 num_iterations = 5000
@@ -36,7 +38,7 @@ T_blank = 0.00175
 noise_sigmasq = 0.2  # TODO adjust this one
 camera_params = T_cycle, T_blank, make_gaussian_model(noise_sigmasq)
 
-# set prior hyperparameters TODO adjust these
+# set prior hyperparameters TODO get these from old HMM analysis?
 intensity_hypers = 1., 1./3  # exponential prior with mean of alpha/beta = 3
 slopey_time_hypers = 2., 4.  # gamma prior peaked near mean of alpha/beta = 1./2
 flat_time_hypers = 3., 2.    # gamma prior peaked near mean of alpha/beta = 3./2
@@ -49,8 +51,8 @@ proposal_params = (1e3, 1e3), 1e3, T_cycle
 
 ### running inference
 
-# make a runner function
-run = model1(num_slopey, prior_params, camera_params, proposal_params, z)
+# make a runner function TODO accept both zR and zG!
+run = model1(num_slopey, prior_params, camera_params, proposal_params, zR)
 
 # run it
 samples = run(num_iterations)
