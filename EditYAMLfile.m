4f2fc5a458aa2f91c6dafef7f010f66b4aafd1b9
Stephanie Johnson <Stephanie.Johnson@ucsf.edu>
2016-04-15 14:50:24 -0700

    Got Yaml read and write working

--- EditYAMLfile.m
+++ EditYAMLfile.m
@@ -1,35 +1,44 @@
-% function success = EditYAMLfile(filename,paramname,paramvalue)
+% function EditYAMLfile(filename,paramname,paramvalue)
 %
 % Steph 4/2016
 
-function success = EditYAMLfile(filename,paramname,paramvalue)
+function EditYAMLfile(filename,paramname,paramvalue)
 
     % load function
     function old_params = load_yml(name)
         if exist(name,'file')
             f = fopen(name,'r');
-            file_all = fread(f);
-%             % How do I know if these fields exist?
-            old_params.start = file_all(blah);
-            old_params.end = file_all(blah);
-            old_params.num_slopey = file_all(blah);
+            file_all = fscanf(f,'%c');
+            old_params = struct();
+            if ~isempty(file_all)
+                [starts,ends] = regexpi(file_all,': \d*');
+                index = 1;
+                for p = 1:length(starts)
+                    field = file_all(index:starts(p)-1);
+                    value = file_all(starts(p)+2:ends(p));
+                    old_params.(field) = value;
+                    clear field value
+                    index = ends(p)+2;
+                end
+            end
             fclose(f);
         else
             old_params = -1;
         end
     end
     % write function
-    function success = write_yml(name,new_params)
+    function write_yml(name,new_params)
+        fields = fieldnames(new_params);
+        output = '';
+        for p = 1:length(fields)
+            if p==length(fields)
+                output = [output,fields{p},':',' ',num2str(new_params.(fields{p}))];
+            else
+                output = [output,fields{p},':',' ',num2str(new_params.(fields{p})),sprintf('\n')];
+            end
+        end
         f = fopen(name,'w');
-        if isfield(old_params.start)
-            
-        end
-        if isfield(old_params.end)
-            
-        end
-        if isfield(old_params.num_slopey)
-            
-        end
+        fprintf(f,'%s',output);
         fclose(f);
     end
 
@@ -39,14 +48,8 @@ function success = EditYAMLfile(filename,paramname,paramvalue)
         clear old
     end
     
-    if strcmpi(paramname,'start')
-        old.start = paramvalue;
-    elseif strcmpi(paramname,'end')
-        old.end = paramvalue;
-    elseif strcmpi(paramname,'num_slopey')
-        old.num_slopey = paramvalue;
-    end
+    old.(paramname) = num2str(paramvalue);
     
-    success = write_yml(filename,old);
+    write_yml(filename,old);
     
 end
