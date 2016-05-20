c81553cd585c11d3d571e540f1ac9262849e622c
Stephanie Johnson <Stephanie.Johnson@ucsf.edu>
2016-05-20 15:37:19 -0700

    Edited to accomodate matts new storage scheme in allresults

--- LoadSlopeyResults.m
+++ LoadSlopeyResults.m
@@ -23,43 +23,25 @@ for k = 1:length(names)
     results{k}.start = double(struct_py.params.start);
     results{k}.end = double(struct_py.params.end);
     
-    samples = struct_py.samples;
-    % Each structure in allresults has 3 fields: params, data, and samples.
-    % Samples is a num_iterations+1-by-3 cell array. If n is
-    % num_iterations:
-    % currstruct.samples{n,1}{1} is a num_slopey+1-by-1 vector, giving the
-    % times in seconds of the start and end of each slopey bit.
-    % currstruct.samples{n,1}{2} is a num_slopey-by-1 vector giving the red
-    % intensity values for each flat bit that separates slopeys.
-    % currstruct.samples{n,2} is a double that gives the offset parameter
-    % for converting from seconds to frames.
-    % currstruct.samples{n,3} is a 2x1 vector that gives the transformation
-    % parameters to obtain real green values instead of idealized ones.
+    num_samples = size(struct_py.times_samples,1);
+    
+    % Each struct in allresults has:
+    % times_samples: num_samples+1 by num_slopey+1 array with times in
+    %     seconds of start and end of each slopey bit
+    % vals_samples: num_samples+1 by num_slopey array with red intensity
+    %     values for each flat bit that separates slopeys
+    % ch2_samples: num_samples by 2 array with transformation parameters to
+    %     obtain real green values from idealized ones
+    % u_samples: num_samples by 1 vector of offsets
+    % params: params struct
+    % data: raw data (num_frames by 2)
 
-    num_samples_to_load = round(max(samples_to_plot,perc_dur_to_analyze*size(samples,1)));
+    num_samples_to_load = round(max(samples_to_plot,perc_dur_to_analyze*num_samples));
     
-    results{k}.ch2_transform = zeros(num_samples_to_load,2);
-    results{k}.offset = zeros(num_samples_to_load,1);
-    try
-        results{k}.times = zeros(num_samples_to_load,size(samples{1,1}{1},1));
-        results{k}.vals = zeros(num_samples_to_load,size(samples{1,1}{2},1));
-        non_vector = 1;
-    catch
-        results{k}.times = zeros(num_samples_to_load,2);
-        results{k}.vals = zeros(num_samples_to_load,2);
-        non_vector = 0;
-    end
-    for j = (size(samples,1)-num_samples_to_load-1):size(samples,1)
-        results{k}.ch2_transform(j,:) = samples{j,3};
-        results{k}.offset(j,:) = samples{j,2};
-        if non_vector == 1
-            results{k}.times(j,:) = samples{j,1}{1};
-            results{k}.vals(j,:) = samples{j,1}{2};
-        else
-            results{k}.times(j,:) = samples{j,1}(1,:);
-            results{k}.vals(j,:) = samples{j,1}(2,:);
-        end
-    end
+    results{k}.ch2_transform = struct_py.ch2_samples(num_samples+1-num_samples_to_load:end,:);
+    results{k}.offset = struct_py.u_samples(num_samples+1-num_samples_to_load:end,:);
+    results{k}.times = struct_py.times_samples(num_samples+1-num_samples_to_load:end,:);
+    results{k}.vals = struct_py.vals_samples(num_samples+1-num_samples_to_load:end,:);
     
-    clear samples
+    clear struct_py
 end
