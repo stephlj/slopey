83ce89b44cfc0d7978e31f9edf6994a10aa23e5d
Matthew Johnson <mattjj@csail.mit.edu>
2016-10-03 15:06:41 -0700

    make RunSlopeyAnalysis.m set MATLAB env var

--- RunSlopeyAnalysis.m
+++ RunSlopeyAnalysis.m
@@ -24,6 +24,7 @@ codedir = cd;
 
 setenv('PYTHONPATH', ['/Users/Steph/code/:', getenv('PYTHONPATH')]);
 setenv('PATH', ['/Users/Steph/miniconda/bin/:', getenv('PATH')]);
+setenv('MATLAB', '1')
 
 % system('make'); % Update anything that needs updating
 cd(symdir)
