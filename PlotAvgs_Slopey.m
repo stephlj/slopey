2fadf03f4d217d597d153081365fb1c047bde506
Stephanie Johnson <Stephanie.Johnson@ucsf.edu>
2016-12-13 16:06:41 -0800

    Now PlotAvgs_Slopey works with pyhsmm results too

--- PlotAvgs_Slopey.m
+++ PlotAvgs_Slopey.m
@@ -1,16 +1,21 @@
 % function PlotAvgs_Slopey(datadir)
 %
-% resultset is any input to ListGoodResults_Slopey.
+% resultset is any input to ListGoodResults_Slopey; if slopey = 1, use
+% output of slopey, else use output of pyhsmm.
 %
 % Steph 11/2016
 
-function PlotAvgs_Slopey(resultset)
+function PlotAvgs_Slopey(resultset,slopey)
 
 num_samples_to_avg = 200;
 
+if ~exist('slopey','var')
+    slopey = 0;
+end
+
 [datasets,labels,legends,colors] = ListGoodResults_Slopey(resultset);
 
-% plot the first three pause durations and the first two translocation
+% plot the first three pause durations and (if slopey) the first two translocation
     % rates
 p_bar_vals = zeros(3,length(datasets));
 t_bar_vals = zeros(2,length(datasets));
@@ -18,14 +23,16 @@ p_err_vals = zeros(3,length(datasets));
 t_err_vals = zeros(2,length(datasets));
 
 for j=1:length(datasets)
-    [means,err_means,rates,rates_errs] = AvgPausesAndTranslocations_Slopey(datasets{j},num_samples_to_avg,labels{j});
+    [means,err_means,rates,rates_errs] = AvgPausesAndTranslocations_Slopey(datasets{j},num_samples_to_avg,labels{j},slopey);
     for p = 1:3
         p_bar_vals(p,j) = means(p);
         p_err_vals(p,j) = err_means(p);
     end
-    for t = 1:2
-        t_bar_vals(t,j) = rates(t);
-        t_err_vals(t,j) = rates_errs(t);
+    if slopey
+        for t = 1:2
+            t_bar_vals(t,j) = rates(t);
+            t_err_vals(t,j) = rates_errs(t);
+        end
     end
     clear means err_means rates rates_errs
 end
@@ -46,16 +53,18 @@ legend(legends)
 ylabel('Duration (sec)')
 set(gca,'Fontsize',14)
 
-figure
-b_t = bar(t_bar_vals);
-for k = 1:length(datasets)
-    b_t(k).FaceColor = colors{k};
+if slopey
+    figure
+    b_t = bar(t_bar_vals);
+    for k = 1:length(datasets)
+        b_t(k).FaceColor = colors{k};
+    end
+    hold on
+    errorbar(xvect(1:2*length(datasets)),reshape(t_bar_vals',1,size(t_bar_vals,1)*size(t_bar_vals,2)),...
+        reshape(t_err_vals',1,size(t_err_vals,1)*size(t_err_vals,2)),'.k')
+    set(gca,'XTick',[1,2])
+    set(gca,'XTickLabel',{'t1';'t2'})
+    legend(legends)
+    ylabel('Translocation rate (bp/sec)')
+    set(gca,'Fontsize',14)
 end
-hold on
-errorbar(xvect(1:2*length(datasets)),reshape(t_bar_vals',1,size(t_bar_vals,1)*size(t_bar_vals,2)),...
-    reshape(t_err_vals',1,size(t_err_vals,1)*size(t_err_vals,2)),'.k')
-set(gca,'XTick',[1,2])
-set(gca,'XTickLabel',{'t1';'t2'})
-legend(legends)
-ylabel('Translocation rate (bp/sec)')
-set(gca,'Fontsize',14)
